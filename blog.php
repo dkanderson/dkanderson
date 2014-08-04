3ca317df3e479d510ddb061bb7b886d6d1cdb515
dkanderson <dkanderson.designs@gmail.com>
2014-08-04 11:00:28 -0400

    basic blog layout

--- blog.php
+++ blog.php
@@ -1,11 +1,40 @@
 <?php $body_class = 'blog'; ?>
 <?php include 'templates/header.php' ?>
         <section id="blog">
-           <div class="intro">
+           <div class="intro blog">
                 <div class="container">
-                    <h1>Ideas, Insights &amp; Introspection</h1>
+                    <h1>Creative Ideas <em class="fancy">&amp;</em> Introspection</h1>
                 </div>
-            </div>            
+            </div>
+            <div class="container">
+                <article class="post">
+                    <h2><a href="#">Some blog post</a></h2>
+                    <div class="post-date">
+                        <p>
+                            <strong>31</strong>
+                            July
+                            <span>2014</span>
+                        </p>
+                    </div>
+                    <a title="Comments" href="#" class="comments-link">
+                        <span>0</span> Comments
+                    </a>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta dolor eu neque aliquet varius. Donec accumsan lorem vel diam blandit, quis tincidunt augue dignissim. Integer tempor, quam eget tempor sollicitudin, nunc mi imperdiet neque, quis elementum tellus dolor eu enim. Integer vel commodo odio, non commodo nibh. Proin sollicitudin eros massa, facilisis mollis neque posuere vitae. Praesent sodales, velit et pharetra euismod, lacus purus venenatis dui, sed dignissim purus tellus quis dolor. Praesent imperdiet rhoncus felis, ac tincidunt felis pretium ut. Aenean non aliquam nulla. Vivamus nec justo odio. Duis eu fringilla mauris. Suspendisse sit amet mi eu elit consectetur rutrum et vel erat. Cras enim tellus, feugiat ut eleifend sit amet, tempor ac nunc. Nam lobortis nibh vel tortor tempus dapibus. Vestibulum ut dictum metus. Aliquam dictum justo eget diam mattis, sed malesuada est mollis.</p>
+
+<p>Aenean ac sodales massa. Morbi ac facilisis turpis, sit amet lobortis felis. Maecenas consectetur mattis quam, quis pulvinar orci vehicula et. Nam porta eros vitae lectus facilisis laoreet. Phasellus vel porttitor mi. Duis ultricies dui dolor. Curabitur ipsum velit, dictum quis auctor in, ornare sit amet elit. Sed leo massa, placerat sit amet neque id, blandit cursus erat. Donec orci urna, ultricies in ante sed, hendrerit vehicula nulla. Morbi luctus tellus quis lorem faucibus pharetra. Ut euismod massa porta enim tincidunt aliquet.</p>
+
+<p>Sed vel odio eu est luctus adipiscing. Quisque quis tortor mauris. Nunc congue interdum nibh, vel fringilla est. Donec ullamcorper imperdiet nisl at commodo. In dapibus arcu nisi, sed elementum magna viverra eu. Sed dignissim, urna eget ullamcorper porta, sem enim adipiscing mauris, sit amet dictum lectus tellus sit amet enim. In quis sapien justo. Cras in pulvinar eros, id convallis ante. </p>
+<p class="rm">
+    <a title="continue reading: Some blog post" href="#">Continue reading <i class="icon ctn-arrow"></i></a>
+</p>
+<p class="meta">
+    <div class="category-list">
+        
+    </div>
+    <div class="tag-list"></div>
+</p>
+                </article>         
+            </div>   
         </section>
         
         <?php include 'templates/footer.php' ?>
