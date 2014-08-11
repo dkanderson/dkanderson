1b54742b2bd6c36a6ad48ba0b1a4e1b96ef0d07c
dkanderson <dkanderson.designs@gmail.com>
2014-08-11 00:52:05 -0400

    project template page

---
+++ project.php
@@ -0,0 +1,27 @@
+<?php $body_class = 'work'; ?>
+<?php include 'templates/header.php' ?>
+<section id="client-banner" style="background-image:url(img/soleil_lg.jpg)">
+	<a class="project-link" href="http://soleillaurent.com">S</a>
+	<div class="project-nav"><a href="#">&laquo;</a><a href="#">&raquo;</a></div>
+</section>
+<section id="soleil_laurent">
+	<div class="container sixteen columns clearfix">
+		<div class="container eight columns">
+			<div class="fold "><img src="img/folio_soleil1.jpg" alt="Soleil Laurent Home Page Screenshot"></div>
+		</div>
+		<div class="container eight columns">
+			<div class="client-meta">
+				<h1>Soleil Laurent</h1>
+				<dl>
+					<dt>url</dt>
+						<dd><a title="Soleil Laurent" href="http://soleillaurent.com">http://soleillaurent.com</a></dd>
+					<dt>tech</dt>
+						<dd>Wordpress, HTML, CSS, Javascript</dd>
+					<dt>description</dt>	
+						<dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer porta semper dolor fermentum lacinia. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse scelerisque sapien sed nisi pharetra pellentesque. Aliquam vestibulum elit vel orci sollicitudin, id adipiscing mi dictum. Nam ut erat eu ligula scelerisque faucibus at in tellus. Cras ut tortor quis libero ornare scelerisque. Quisque in risus at orci rutrum scelerisque vitae vitae mauris. </dd>
+				</dl>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include 'templates/footer.php' ?>
