a916ee7cd0e1280801b5d32a115a0fe8157792db
dkanderson <dkanderson.designs@gmail.com>
2014-08-11 00:47:11 -0400

    animation triggers and cleanup for mobile nav

--- js/main.js
+++ js/main.js
@@ -37,6 +37,26 @@ var DKA = {
           return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
 
         };
+
+        $('#mob-trigger').click(function(){
+            $('#mob-trigger').addClass('open');
+            $('body').append('<div class="menu-bg-cover"><div class="menu-container"></div></div><div class="overlayW" />');
+
+            $('.screen-nav').clone().appendTo('.menu-container').removeClass('screen-nav').addClass('mobi-nav');
+            setTimeout(function() {
+                $('.menu-container').addClass('in');
+                $('.overlayW').addClass('active');
+            }, 100);
+
+            $('.menu-bg-cover').click(function(){
+              $('.menu-container').removeClass('in').addClass('out');
+              $('#mob-trigger').removeClass('open');
+              setTimeout(function(){
+                  $('.menu-bg-cover').remove();
+                  $('.overlayW').remove();
+              }, 1000);
+            });
+        });
     }
   },
   // Home page
