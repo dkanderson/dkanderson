50fc1fa3ffa5a47d36511d87e1ff69544b1c0e24
dkanderson <dkanderson.designs@gmail.com>
2014-08-11 14:57:41 -0400

    dont remember honestly

--- js/main.js
+++ js/main.js
@@ -40,14 +40,13 @@ var DKA = {
 
         $('#mob-trigger').click(function(){
             $('#mob-trigger').addClass('open');
-            $('body').append('<div class="menu-bg-cover"><div class="menu-container"></div></div><div class="overlayW" />');
+            $('body').append('<div class="menu-bg-cover"><div class="menu-container"></div></div><div class="overlayW"></div>');
 
             $('.screen-nav').clone().appendTo('.menu-container').removeClass('screen-nav').addClass('mobi-nav');
             setTimeout(function() {
                 $('.menu-container').addClass('in');
                 $('.overlayW').addClass('active');
             }, 100);
-
             $('.menu-bg-cover').click(function(){
               $('.menu-container').removeClass('in').addClass('out');
               $('#mob-trigger').removeClass('open');
